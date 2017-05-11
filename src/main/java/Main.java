2081db8af9b8a6afc0021412b20ddb1bef33942e
Diana Tokarska <tokarskadiana@gmail.com>
2017-05-11 18:45:58 +0200

    Refactor Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,5 @@
 import controller.EventController;
-import spark.Request;
-import spark.Response;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
 
 import  static spark.Spark.*;
 
@@ -22,10 +16,6 @@ public class Main {
 
         // Always add generic routes to the end
         get("/", EventController::renderProducts, new ThymeleafTemplateEngine());
-        // Equivalent with above
-        get("/index", (Request req, Response res) -> {
-            return new ThymeleafTemplateEngine().render( EventController.renderProducts(req, res) );
-        });
     }
 
 
