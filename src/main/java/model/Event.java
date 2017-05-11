c6af2f0f4bdd485067e5cc0fc275b21a34697956
Diana Tokarska <tokarskadiana@gmail.com>
2017-05-11 19:08:07 +0200

    Add constructor to Event model

--- src/main/java/model/Event.java
+++ src/main/java/model/Event.java
@@ -1,5 +1,22 @@
 package model;
 
 
+import java.util.Date;
+
 public class Event {
+    Integer id;
+    String name;
+    String description;
+    String category;
+    Date date;
+
+    Event(Integer id, String name, String description,
+          String category, Date date){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.date = date;
+    }
+
 }
