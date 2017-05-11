0cafb459dd2f2c6e85ebf59fa86c79d571e94c1e
Diana Tokarska <tokarskadiana@gmail.com>
2017-05-11 19:44:34 +0200

    Add sample events

--- src/main/java/dao/EventDao.java
+++ src/main/java/dao/EventDao.java
@@ -3,6 +3,7 @@ package dao;
 import model.Event;
 
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 
@@ -10,6 +11,14 @@ public class EventDao {
 
     public List<Event> getAll() {
         List<Event> events = new ArrayList<>();
+        events.add(new Event(1, "Codecool cinema",
+                "Cinema evening",
+                "Culture",
+                new GregorianCalendar(2017, 6, 15)));
+        events.add(new Event(2, "Codecool hakaton",
+                "24 hours of codding",
+                "Android developing",
+                new GregorianCalendar(2017, 6, 1)));
         return events;
     }
 }
