63269a5fed1e1116d21e2cc4b04f4091a851d97a
Diana Tokarska <tokarskadiana@gmail.com>
2017-05-12 14:02:10 +0200

    Get events from Dao

--- src/main/java/controller/EventController.java
+++ src/main/java/controller/EventController.java
@@ -1,22 +1,20 @@
 package controller;
 
 import dao.EventDao;
+import model.Event;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-/**
- * Created by rafalstepien on 28/04/2017.
- */
 public class EventController {
     public static ModelAndView renderProducts(Request req, Response res) {
-        //Get events from database by Dao
-
+        EventDao eventDao = new EventDao();
         Map params = new HashMap<>();
-        params.put("eventContainer", "Codecool cinema");
+        params.put("eventContainer", eventDao.getAll());
         return new ModelAndView(params, "product/index");
     }
 }
