a30f2fb0c774856843fd5485954ed3c35067b78e
Diana Tokarska <tokarskadiana@gmail.com>
2017-05-17 01:54:54 +0200

    Create tables when server starts

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 import controller.EventController;
+import dao.SqliteJDSCConnector;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
+import java.sql.SQLException;
 
 import  static spark.Spark.*;
 
@@ -10,6 +13,11 @@ public class Main {
         exception(Exception.class, (e, req, res) -> e.printStackTrace());
         staticFileLocation("/public");
         port(8888);
+        try {
+            SqliteJDSCConnector.createTables();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         // Always start with more specific routes
         get("/hello", (req, res) -> "Hello World");
