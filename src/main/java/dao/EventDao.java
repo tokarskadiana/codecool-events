daf6af6b75265dc1610b62feeb32415fb7540579
Diana Tokarska <tokarskadiana@gmail.com>
2017-05-17 01:49:20 +0200

    Add reading events from database

--- src/main/java/dao/EventDao.java
+++ src/main/java/dao/EventDao.java
@@ -1,8 +1,13 @@
 package dao;
 
 import model.Event;
+import org.thymeleaf.util.DateUtils;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
 
@@ -11,14 +16,27 @@ public class EventDao {
 
     public List<Event> getAll() {
         List<Event> events = new ArrayList<>();
-        events.add(new Event(1, "Codecool cinema",
-                "Cinema evening",
-                "Culture",
-                new GregorianCalendar(2017, 6, 15)));
-        events.add(new Event(2, "Codecool hakaton",
-                "24 hours of codding",
-                "Android developing",
-                new GregorianCalendar(2017, 6, 1)));
+        try {
+            Statement statement = SqliteJDSCConnector.makeConnection().createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * from events");
+            while(rs.next()){
+                Event event = new Event(
+                        rs.getString("name"),
+                        rs.getString("description"),
+                        rs.getString("category"),
+                        rs.getDate("date"));
+                event.setId(rs.getInt("id"));
+                events.add(event);
+            }
+        } catch (SQLException e) {
+            System.out.println("Connection failed");
+            e.printStackTrace();
+        }
         return events;
+
+    }
+
+    public void add(Event event){
+        //add to data base
     }
 }
